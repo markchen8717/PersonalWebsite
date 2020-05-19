2d5c095e6034c7fdf49a920d6932eb1862c09da3
markchen8717 <29904105+markchen8717@users.noreply.github.com>
2020-05-18 21:24:37 -0400

    Netlify

--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -78,26 +78,45 @@ export default function ContactForm() {
         //         </Grid>
         //     </Container>
         // </form>
-        <form method="post" netlify-honeypot="bot-field" data-netlify="true">
-            <input type="hidden" name="bot-field" />
-            <label>
-                Name
-    <input type="text" name="name" id="name" />
-            </label>
-            <label>
-                Email
-    <input type="email" name="email" id="email" />
-            </label>
-            <label>
-                Subject
-    <input type="text" name="subject" id="subject" />
-            </label>
-            <label>
-                Message
-    <textarea name="message" id="message" rows="5" />
-            </label>
-            <button type="submit">Send</button>
-            <input type="reset" value="Clear" />
-        </form>
+        <form
+        name="contact"
+        method="post"
+        action="/thanks/"
+        data-netlify="true"
+        data-netlify-honeypot="bot-field"
+        
+      >
+        {/* The `form-name` hidden field is required to support form submissions without JavaScript */}
+        <input type="hidden" name="form-name" value="contact" />
+        <p hidden>
+          <label>
+            Don’t fill this out: <input name="bot-field"  />
+          </label>
+        </p>
+        <p>
+          <label>
+            Your name:
+            <br />
+            <input type="text" name="name"  />
+          </label>
+        </p>
+        <p>
+          <label>
+            Your email:
+            <br />
+            <input type="email" name="email" />
+          </label>
+        </p>
+        <p>
+          <label>
+            Message:
+            <br />
+            <textarea name="message" />
+          </label>
+        </p>
+        <p>
+          <button type="submit">Send</button>
+        </p>
+      </form>
     );
 }
