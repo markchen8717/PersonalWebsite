eecf10929a9504cc198c637dd774df2f0905d6bf
markchen8717 <29904105+markchen8717@users.noreply.github.com>
2020-05-18 21:11:26 -0400

    Netlify

--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -78,19 +78,26 @@ export default function ContactForm() {
         //         </Grid>
         //     </Container>
         // </form>
-        <form name="contact" method="POST" netlify-honeypot="bot-field" data-netlify="true">
-  <p class="hidden">
-    <label>Don’t fill this out if you're human: <input name="bot-field" /></label>
-  </p>
-  <p>
-    <label>Email: <input type="text" name="email" /></label>
-  </p>
-  <p>
-    <label>Message: <textarea name="message"></textarea></label>
-  </p>
-  <p>
-    <button type="submit">Send</button>
-  </p>
-</form>
+        <form method="post" netlify-honeypot="bot-field" data-netlify="true">
+            <input type="hidden" name="bot-field" />
+            <label>
+                Name
+    <input type="text" name="name" id="name" />
+            </label>
+            <label>
+                Email
+    <input type="email" name="email" id="email" />
+            </label>
+            <label>
+                Subject
+    <input type="text" name="subject" id="subject" />
+            </label>
+            <label>
+                Message
+    <textarea name="message" id="message" rows="5" />
+            </label>
+            <button type="submit">Send</button>
+            <input type="reset" value="Clear" />
+        </form>
     );
 }
